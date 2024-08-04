48ebbdb03cbd0668805448f1f4766fefe63aa2e3
mulutu <mulutujackson@gmail.com>
2024-08-04 16:27:35 +0300

    First Commit

---
+++ storage/framework/views/d711032ed2ada25b87c2d5e9aba2c781.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>
+
+<?php /**PATH /home/accufy_erp/public/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/footer.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/e6d9a2b8efc93c6a964cd7f684a3e34d.php
@@ -0,0 +1,3 @@
+<?php echo e($slot); ?>: <?php echo e($url); ?>
+
+<?php /**PATH /home/accufy_erp/public/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/button.blade.php ENDPATH**/ ?>
